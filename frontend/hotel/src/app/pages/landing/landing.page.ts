b440ddded878cbe19d2e90d98c67fac0d8289cff
GiovaGrifo <giovanni.grifo04@gmail.com>
2025-06-15 19:31:29 +0200

    First draft of landing page

--- frontend/hotel/src/app/pages/landing/landing.page.ts
+++ frontend/hotel/src/app/pages/landing/landing.page.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardContent, IonCardHeader, IonCard, IonCardTitle } from '@ionic/angular/standalone';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { IonContent, IonTitle, IonButton, IonText, IonGrid, IonRow, IonCol, IonCard, IonCardTitle, IonCardContent, IonCardHeader } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.page.html',
   styleUrls: ['./landing.page.scss'],
   standalone: true,
-  imports: [IonContent,IonTitle, CommonModule, FormsModule]
+  imports: [IonCardHeader, IonCardContent, IonCardTitle, IonCard, IonCol, IonRow, IonGrid, IonContent, IonTitle, IonText, CommonModule, NgOptimizedImage, IonButton] //Angular raccomanda di utilizzare NgOptimizedImage per gestire le immagini al meglio
 })
 export class LandingPage implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
